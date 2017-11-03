517d9c260f74f0fae14e8d32d0ca5b1c5d1274a1
Michael Doo <michael.c.doo@gmail.com>
2017-11-03 11:56:17 -0400

    Destiny 2: Add on_start

--- plugins/destiny2.py
+++ plugins/destiny2.py
@@ -1,9 +1,45 @@
+import glob
+import pickle
 import sys
 import traceback
+import urllib.parse
+
+import requests
 
 from cloudbot import hook
+from cloudbot.event import EventType
 from . import destiny_manifest
 
+BASE_URL = 'https://www.bungie.net/Platform/'
+CLASS_TYPES = {0: 'Titan ', 1: 'Hunter ', 2: 'Warlock ', 3: ''}
+
+@hook.on_start()
+def load_cache(bot):
+    '''Load in our pickle manifest and the Headers'''
+
+    # Set the API key
+    global HEADERS
+    HEADERS = {'X-API-Key': bot.config.get('api_keys', {}).get('destiny', None)}
+
+    # Get the manifest pickle if it exists
+    pickles = glob.glob('destiny_manifest_*.pickle')
+
+    # If more than one pickle, only keep latest one
+    if len(pickles) == 1:
+        filename = pickles[0]
+    elif len(pickles) > 1:
+        os.remove(sorted(pickles)[0])
+        filename = sorted(pickles)[-1]
+    else:
+        api_key = bot.config.get('api_keys', {}).get('destiny', None)
+        filename = destiny_manifest.gen_manifest_pickle(api_key)
+
+    try:
+        with open(filename, 'rb') as f:
+            global MANIFEST
+            MANIFEST = pickle.load(f)
+    except EOFError:
+        MANIFEST = {}
 
 @hook.periodic(24 * 60 * 60, initial_interval=10)
 def check_manifest(bot):
