d3abf4c66ef7c3810ef69f06a3dd83123a476d9e
cerulliulliulli <cerulliulliulli@users.noreply.github.com>
2019-03-02 19:18:46 +0000

    Refactoring script starting with a URL fix by replacing all non-alphanumeric characters with hyphens in the game title.

--- plugins/opencritic.py
+++ plugins/opencritic.py
@@ -1,5 +1,6 @@
 import requests
 import datetime
+import re
 import fuzzywuzzy
 from fuzzywuzzy import process
 
@@ -8,98 +9,38 @@ from cloudbot import hook
 @hook.command('oc')
 def oc(text):
     url = 'http://api.opencritic.com/api/'
-    searchURL = url + 'game/search'
-    scoreURL = url +  'game/score'
+    searchUrl = url + 'game/search'
+    scoreUrl = url + 'game/score'
 
     headers = {
-    'content-type': "application/x-www-form-urlencoded",
-    'cache-control': "no-cache"
+        'content-type':"application/x-www-form-urlencoded",
+        'cache-control':"no-cache"
     }
 
     searchQuery = {
-    'criteria': text
+        'criteria':text
     }
-  
-    response = requests.request("GET", searchURL, headers = headers, params = searchQuery).json()
+
+    response = requests.request("GET", searchUrl, headers = headers, params = searchQuery).json()
 
     choices = [item['name'] for item in response]
-    best = process.extract(text,choices, limit=1)[0][0]
-        
+    bestChoice = process.extract(text, choices, limit=1)[0][0]
+
     for item in response:
-        if item['name'] == best:
-            gameID = item['id']
+        if item['name'] == bestChoice:
+            gameId = item['id']
             gameTitle = item['name']
-            gameLink = 'http://opencritic.com/game/' + str(gameID) + '/'
+            gameUrl = 'http://opencritic.com/game/' + str(gameId) + '/' + re.sub('[^0-9a-zA-Z]+', '-', item['name']).lower()
 
     scoreQuery = {
-    'id': gameID
+        'id':gameId
     }
 
-    response = requests.request("GET", scoreURL, headers = headers, params = scoreQuery).json()
+    response = requests.request("GET", scoreUrl, headers = headers, params = scoreQuery).json()
 
-    try:
+    try: 
         gameScore = round(float(response['score']))
     except:
         return '\x02{}\x02 does not have an average score yet.'.format(gameTitle)
     else:
-        return '\x02{}\x02 - \x02Score:\x02 {} - {}'.format(gameTitle, gameScore, gameLink)
-
-@hook.command('octop')
-def octop(text):
-    url = 'http://api.opencritic.com/api/game/filter'
-
-    headers = {
-    'content-type': "application/json",
-    }
-
-    data = '{"limit": 5, "orderBy": "score", "startDate": "2017-1-1"}'
-
-    output = []
-
-    response = requests.post(url, headers = headers, data = data)
-
-    for i in response.json():
-        output.append('\x02{}\x02: {}'.format(i['name'], round(float(i['score']))))
-
-    return ', '.join(output)
-
-@hook.command('ocup')
-def ocup(text):
-    url = 'http://api.opencritic.com/api/game/filter'
-
-    if text.lower() in {'switch', 'nintendo', 'nintendo switch'}:
-        text = '32'
-    elif text.lower() in {'sony', 'playstation', 'playstation 4', 'ps4', 'psn'}:
-        text = '6'
-    elif text.lower() in {'microsoft', 'ms', 'xbox', 'xbox one', 'xbox 1', 'xb1', 'xbl'}:
-        text = '7'
-    elif text.lower() in {'windows', 'pc'}:
-        text = '27'
-    elif text.lower() in {'3ds'}:
-        text = '36'
-    elif text.lower() in {'vive', 'htc', 'htc vive'}:
-        text = '30'
-    elif text.lower() in {'rift', 'oculus', 'oculus rift'}:
-        text = '29'
-    elif text.lower() in {'vr'}:
-        text = '29,30'
-    elif text.lower() in {'vita', 'ps vita', 'psp'}:
-        text = '33'
-    
-    startDate = datetime.datetime.now()
-    
-    headers = {
-    'content-type': "application/json",
-    }
-
-    data = '{"Platforms": [' + text + '], "limit": 5, "orderBy": "timeAscending", "startDate": "' + str(startDate) + '-3-24", "minTime": true}'
-
-    output = []
-
-    response = requests.post(url, headers = headers, data = data)
-
-    for i in response.json():
-        output.append('\x02{}\x02: {}'.format(i['name'], i['releaseDate'][:10]))
-
-    return ', '.join(output)
-
+        return '\x02{}\x02 - \x02Score:\x02 {} - {}'.format(gameTitle, gameScore, gameUrl)
