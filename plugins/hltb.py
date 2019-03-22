c6490d94f4b05e2dc4191bd4bff6f87ab94e1fbc
cerulliulliulli <cerulliulliulli@users.noreply.github.com>
2019-03-22 23:16:54 +0000

    URL is not search_results instead of search_main, along with HTTPS for POST requests instead of HTTP (similar to the fix for OpenCritic)

--- plugins/hltb.py
+++ plugins/hltb.py
@@ -4,31 +4,32 @@ from cloudbot import hook
 
 @hook.command("hltb")
 def hltb(text):
-    url = "http://howlongtobeat.com/search_main.php?page=1"
-    payload = {"queryString": text,
-               "t": "games",
-               "sorthead": "popular",
-               "sortd": "Normal Order",
-               "length_type": "main",
-               "detail": "0"}
-    test = {'Content-type': 'application/x-www-form-urlencoded',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.97 Safari/537.36',
-            'origin': 'https://howlongtobeat.com',
-            'referer': 'https://howlongtobeat.com'}
+    url = "https://howlongtobeat.com/search_results.php?page=1"
+    payload = {'queryString': text,
+               't': 'games',
+               'sorthead': 'popular',
+               'sortd': 'Normal Order',
+               'length_type': 'main',
+               'detail': '0'}
+    headers = {'Content-type': 'application/x-www-form-urlencoded',
+               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+               'Origin': 'https://howlongtobeat.com',
+               'Referer': 'https://howlongtobeat.com/'}
 
     session = requests.Session()
-    session.post(url, headers=test, data=payload)
-    r = session.post(url, headers=test, data=payload)
+    session.post(url, headers=headers, data=payload)
+    r = session.post(url, headers=headers, data=payload)
 
     if len(r.content) < 250:
         return "No results."
 
     bs = BeautifulSoup(r.content, 'lxml')
-    first = bs.findAll("div", {"class": "search_list_details"})[0]
+
+    first = bs.findAll("div", {"class": "search_list_details"})[0]    
     output = ["\x02{}\x02 -".format(first.a.text)]
     rest = first.findAll('div')
     
-    entries = {2: '\x02{}\x02:', 3: '{}', 5: '\x02{}\x02:', 6: '{}', 8: '\x02{}\x02:', 9: '{}'}
+    entries = {2: '\x02{}\x02:', 3: '{}', 6: '\x02{}:\x02', 7: '{}'}
     for i in range(len(rest)):
         if i in entries:
             output.append(entries[i].format(rest[i].text))
